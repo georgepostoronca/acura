c1a03cc1ba492ea6e4d7de81725ef4dcf01a7700
George <george.postoronca@gmail.com>
2021-03-10 22:54:25 +0300

    - блок header(desktop/adaptive)

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,8 @@
-import "./import/modules";
-import "./import/components";
+import SlimSelect from 'slim-select'
+
+let selects = [].slice.call(document.querySelectorAll("select"));
+selects.forEach((item) => {
+  new SlimSelect({
+    select: item
+  });
+})
